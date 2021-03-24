c01888c8a5782217001e4d1da57267376a880216
Werner Freytag <freytag@gmx.de>
2021-03-24 16:17:51 +0100

    handle nil in debugDescription

--- Sources/SwiftToolbox/DefaultStringInterpolation+Extension.swift
+++ Sources/SwiftToolbox/DefaultStringInterpolation+Extension.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 public extension DefaultStringInterpolation {
-    mutating func appendInterpolation<T>(debugDescription value: T) {
+    mutating func appendInterpolation<T>(debugDescription value: T?) {
+        guard let value = value else { appendInterpolation("nil"); return }
+
         if let debugDescription = (value as? CustomDebugStringConvertible)?.debugDescription {
             appendInterpolation(debugDescription)
         } else {
