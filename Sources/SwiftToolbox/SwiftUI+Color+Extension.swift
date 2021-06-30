b9780dce4acd56e50de30cdd0a775d6554062576
Werner Freytag <freytag@gmx.de>
2021-06-30 16:09:36 +0200

    add conditions for tvOS, watchOS

--- Sources/SwiftToolbox/SwiftUI+Color+Extension.swift
+++ Sources/SwiftToolbox/SwiftUI+Color+Extension.swift
@@ -4,7 +4,7 @@
 
 import SwiftUI
 
-@available(iOS 13, macOS 10.15, *)
+@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 public extension SwiftUI.Color {
     init(hex: Int) {
         let components = (
