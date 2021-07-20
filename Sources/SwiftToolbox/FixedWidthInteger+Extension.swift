7720259a2f1bb6677dc7e87bffaaafbba8a8abaf
Werner Freytag <freytag@gmx.de>
2021-07-20 17:37:31 +0200

    add numberOfDigits to FixedWidthInteger

---
+++ Sources/SwiftToolbox/FixedWidthInteger+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright © 2021 Werner Freytag. All rights reserved.
+//
+
+import Foundation
+
+public extension FixedWidthInteger {
+    /// returns the number of digits an Int has
+    var numberOfDigits: Int {
+        guard self != 0 else { return 1 }
+        return Int(log10(abs(Double(self)))) + 1
+    }
+}
